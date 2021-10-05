2a62b40e3a51f83222ecc95a47cd8b71cbe6e112
Pineapple <pineapple_cpp@163.com>
2021-10-05 09:13:28 +0800

    使用CSRFProtect实现CSRF保护

--- blog/__init__.py
+++ blog/__init__.py
@@ -6,11 +6,12 @@ import os
 
 import click
 from flask import Flask, render_template
+from flask_wtf.csrf import CSRFError
 
 from blog.blueprints.admin import admin_bp
 from blog.blueprints.auth import auth_bp
 from blog.blueprints.blog import blog_bp
-from blog.extensions import bootstrap, db, ckeditor, mail, moment, login_manager
+from blog.extensions import bootstrap, db, ckeditor, mail, moment, login_manager, csrf
 from blog.models import Admin, Category, Post, Comment, Link
 from blog.settings import config
 
@@ -40,6 +41,7 @@ def register_extensions(app):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    csrf.init_app(app)
     ckeditor.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
@@ -78,6 +80,10 @@ def register_errors(app):
     @app.errorhandler(500)
     def internal_server_error(e):
         return render_template('errors/500.html'), 500
+
+    @app.errorhandler(CSRFError)
+    def handle_csrf_error(e):
+        return render_template('errors/400.html', description=e.description), 400
 
 
 def register_commands(app):

29021688ee63576f8e54f5e4a0f003c063b48a67
Pineapple <pineapple_cpp@163.com>
2021-10-05 10:47:01 +0800

    编写博客后台

--- blog/__init__.py
+++ blog/__init__.py
@@ -6,6 +6,7 @@ import os
 
 import click
 from flask import Flask, render_template
+from flask_login import current_user
 from flask_wtf.csrf import CSRFError
 
 from blog.blueprints.admin import admin_bp
@@ -65,7 +66,13 @@ def register_template_context(app):
         admin = Admin.query.first()
         categories = Category.query.order_by(Category.name).all()
         links = Link.query.order_by(Link.name).all()
-        return dict(admin=admin, categories=categories, links=links)
+        if current_user.is_authenticated:
+            unread_comments = Comment.query.filter_by(reviewed=False).count()
+        else:
+            unread_comments = None
+        return dict(
+            admin=admin, categories=categories,
+            links=links, unread_comments=unread_comments)
 
 
 def register_errors(app):
